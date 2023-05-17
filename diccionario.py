867165d34b761b40b3ce9a013aa5b002a2739637
keneroli18 <129073693+keneroli18@users.noreply.github.com>
2023-05-17 17:31:58 -0500

    ejercicio
    desktop

---
+++ diccionario.py
@@ -0,0 +1,6 @@
+dictionary = {"marlon": "mensaje", "wilfrank": "decir", "miguel": "diciendo"}
+phone_numbers = {'instructor': 3201829345, 'kener': 3208991489}
+print(dictionary)
+print(phone_numbers)
+
+ 
---
+++ tuplas.py
@@ -0,0 +1,20 @@
+#ejercicio1
+# subjects = ("Matemáticas", "Física", "Química", "Historia", "Lengua")
+# print(subjects)
+
+#ejercicio2
+# tuple_1 = (1, 2, 4, 8)
+# tuple_2 = 1., .15, .125, .1200
+# print(tuple_1)
+# print(tuple_2)
+#ejercicio3
+my_tuple = (1, 10, 100)
+
+t1 = my_tuple + (1000, 10000)
+t2 = my_tuple * 3
+
+print(len(t2))
+print(t1)
+print(t2)
+print(10 in my_tuple)
+print(-10 not in my_tuple)
